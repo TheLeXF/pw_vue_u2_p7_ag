8ff35105735ea145043e9054942f3467db044b08
Alexis <ferguanoluisa99@hotmail.com>
2024-12-19 00:45:37 +0000

    Taller 17

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome Alexis Guanoluisa"/>
+  <Contador/>
+  <Contador/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Contador from './components/Contador.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Contador
   }
-}
+};
 </script>
 
 <style>
